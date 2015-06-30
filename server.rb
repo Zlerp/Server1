0533f68cc5b9e0fa6d550816b2195b78af0e5a6d
Zlerp <zhpdeals@gmail.com>
2015-06-30 12:24:03 -0400

    Second Commit

--- server.rb
+++ server.rb
@@ -1,17 +1,17 @@
 require 'sinatra'
 
-# go to localhost:PORT (127.0.0.1:PORT) and with sinatra, you have a webserver.
+# # go to localhost:PORT (127.0.0.1:PORT) and with sinatra, you have a webserver.
 
-get '/' do
-  "Hi friends! This is my website mothja fucka!"
-end
+# get '/' do
+#   "Hi friends! This is my website mothja fucka!"
+# end
 
-# 2 arguments, takes a path string, and then it takes a block.
+# # 2 arguments, takes a path string, and then it takes a block.
 
-get '/blah' do
-  "Hello World"
-end
+# get '/blah' do
+#   "Hello World"
+# end
 
-get '/sinatra' do
-	"Hello Sinatra"
-end
+# get '/sinatra' do
+# 	"Hello Sinatra"
+# end
